5702b172c22bddac0d85dd4f7b6e34836cd8b614
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-08-27 17:53:10 -0400

    Remove most of dRICH material for new dev strategy

--- MOBO-tools/ProjectUtils/ePICUtils/runOverlapCheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-checkOverlaps -c ${DETECTOR_PATH}/${DETECTOR_CONFIG}.xml > overlap_log.txt 2>&1
--- MOBO-tools/ProjectUtils/ePICUtils/runOverlapCheck_jobGeo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-checkOverlaps -c ${DETECTOR_PATH}/${DETECTOR_CONFIG}_$1.xml > $AIDE_HOME/log/overlaps/overlap_log_$1.txt 2>&1
