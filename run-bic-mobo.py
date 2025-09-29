245cc9ea0274b37d4a25664bc5e1243dddc5b05d
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-09-29 17:38:14 -0400

    Get BIC-MOBO running with dummy objective

--- run-bic-mobo.py
+++ run-bic-mobo.py
@@ -10,6 +10,7 @@
 from ax.service.ax_client import AxClient, ObjectiveProperties
 from scheduler import AxScheduler, JobLibRunner
 
+import AID2ETestTools as att
 import EICMOBOTestTools as emt
 
 def counter(function):
@@ -24,8 +25,8 @@ def counter(function):
     wrapped.calls = 0
     return wrapped
 
-@counter
-def RunObjectives(parameterization):
+#@counter
+def RunObjectives(*args, **kwargs):
     # TODO
     #   - translate call # into tag
     #   - create trial manager and script
@@ -35,14 +36,44 @@ def RunObjectives(parameterization):
     #   - create and return dictionary
     #     of objectives
 
+    # dummy objective
+    objective = 0
+    for parVal in kwargs.values():
+        objective -= parVal
+    return {"ElectronEnergyResolution" : objective}
+
 def main():
     # TODO
-    #   - load config files
-    #   - translate parameters, objectives into
-    #     ax-compliant ones
-    #   - initialize ax client
-    #   - set up scheduler and run
     #   - save everything for analysis
+
+    # load config files
+    cfg_run = emt.ReadJsonFile("run_config.json")
+    cfg_exp = emt.ReadJsonFile("problem_config.json")
+    cfg_par = emt.ReadJsonFile("parameters_config.json")
+    cfg_obj = emt.ReadJsonFile("objectives_config.json")
+
+    # translate parameter, objective options
+    # into ax-compliant ones
+    ax_pars = att.ConvertParamConfig(cfg_par)
+    ax_objs = att.ConvertObjectConfig(cfg_obj)
+
+    # create ax client
+    ax_client = AxClient()
+    ax_client.create_experiment(
+        name = cfg_exp["problem_name"],
+        parameters = ax_pars,
+        objectives = ax_objs
+    )
+
+    # set up scheduler
+    #   - TODO add switch to toggle slurm running
+    runner    = JobLibRunner(n_jobs = -1)
+    scheduler = AxScheduler(ax_client, runner)
+    scheduler.set_objective_function(RunObjectives)
+
+    # run and report best parameters
+    best = scheduler.run_optimization(max_trials = 10)
+    print("Optimization complete! Best parameters:\n", best)
 
 if __name__ == "__main__":
    main()
