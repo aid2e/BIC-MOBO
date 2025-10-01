8f4434762518e95a9b0a0c8c532fb1411390ce76
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-10-01 12:39:23 -0400

    Complete stage 1 wrapper script

--- run-bic-mobo.py
+++ run-bic-mobo.py
@@ -8,19 +8,30 @@
 # =============================================================================
 
 import os
+import pickle
+import ROOT
 import subprocess
 
 from ax.service.ax_client import AxClient, ObjectiveProperties
+from ax.service.utils.report_utils import exp_to_df
 from scheduler import AxScheduler, JobLibRunner
 
 import AID2ETestTools as att
 import EICMOBOTestTools as emt
 
 def RunObjectives(*args, **kwargs):
-    # TODO
-    #   - extract objectives from output
-    #   - create and return dictionary
-    #     of objectives
+    """RunObjectives
+
+    Runs trial (simulation, reconstruction,
+    and all analyses) for provided set of
+    updated parameters.
+
+    Args:
+      args:   any positional arguments
+      kwargs: any keyword arguments
+    Returns:
+      dictionary of objectives and their values
+    """
 
     # create tag for trial
     tag = "AxTrial" + str(RunObjectives.counter)
@@ -33,9 +44,9 @@ def RunObjectives(*args, **kwargs):
 
     # determine paths to config files
     #   -- FIXME this is brittle!
-    run_path  = main_path + "/run_config.json"
-    par_path  = main_path + "/parameters_config.json"
-    obj_path  = main_path + "/objectives_config.json"
+    run_path  = main_path + "/configuration/run_config.json"
+    par_path  = main_path + "/configuration/parameters_config.json"
+    obj_path  = main_path + "/configuration/objectives_config.json"
 
     # parse run config to extract path to eic-shell 
     cfg_run   = emt.ReadJsonFile(run_path)
@@ -50,18 +61,39 @@ def RunObjectives(*args, **kwargs):
     script, ofiles = trial.MakeTrialScript(tag, kwargs)
     subprocess.run([eic_shell, "--", script])
 
-    # dummy objective
-    objective = 0
-    for parVal in kwargs.values():
-        objective = objective - parVal
-    return {"ElectronEnergyResolution" : objective}
+    # extract electron resolution 
+    ofResEle = ROOT.TFile.Open(ofiles["ElectronEnergyResolution"])
+    fResEle  = ofResEle.Get("fEneRes")
+    eResEle  = fResEle.GetParameter(2)
+
+    # return dictionary of objectives
+    return {
+        "ElectronEnergyResolution" : eResEle
+    }
 
 # make sure trial counter starts at 0
 RunObjectives.counter = 0
 
 def main():
-    # TODO
-    #   - save everything for analysis
+    """main
+
+    Wrapper to run BIC-MOBO. The model
+    and generation strategy are saved
+    to both JSON and CSV files (model)
+    and pickle files (generation) for
+    downstream analysis.
+
+    User can
+    specify which runner to use with
+    the -r option:
+
+      joblib -- use joblib runner (default)
+      slurm  -- use slurm runner (TODO)
+      panda  -- use panda runner (TODO)
+
+    Args:
+      -r: specify runner (optional)
+    """
 
     # load relevant config files
     cfg_exp = emt.ReadJsonFile("problem_config.json")
@@ -88,8 +120,27 @@ def main():
     scheduler.set_objective_function(RunObjectives)
 
     # run and report best parameters
-    best = scheduler.run_optimization(max_trials = 2)
+    best = scheduler.run_optimization(max_trials = 720)
     print("Optimization complete! Best parameters:\n", best)
+
+    # create paths to output files
+    oPathBase = cfg_exp["OUTPUT_DIR"] + "/" + cfg_exp["problem_name"]
+    oPathCSV  = oPathBase + "_exp_out.csv"
+    oPathJson = oPathBase + "_exp_out.json"
+    oPathPikl = oPathBase + "_gen_out.pkl"
+
+    # grab experiment and generation strategy
+    # for output
+    exp   = ax_client._experiment
+    gen   = ax_client._generation_strategy
+    dfExp = exp_to_df(exp)
+
+    # save outcomes and experiment
+    # for downstream analysis
+    dfExp.to_csv(oPathCSV)
+    ax_client.save_to_json_file(oPathJson)
+    with open(oPathPikl, 'wb') as file:
+        pickle.dump(gen.model, file)
 
 if __name__ == "__main__":
    main()
