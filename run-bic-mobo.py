c60f8883dbb6302cd031b84e877bd261274dd0a0
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-10-09 18:44:16 -0400

    Add toggle for runner selection, move objective function to script for script-based objectives

--- run-bic-mobo.py
+++ run-bic-mobo.py
@@ -7,75 +7,20 @@
 #    running the BIC-MOBO problem.
 # =============================================================================
 
-import datetime
+import argparse
 import os
 import pickle
-import re
-import subprocess
 
 from ax.service.ax_client import AxClient, ObjectiveProperties
 from ax.service.utils.report_utils import exp_to_df
-from scheduler import AxScheduler, JobLibRunner
+from scheduler import AxScheduler, JobLibRunner, SlurmRunner
 
 import AID2ETestTools as att
 import EICMOBOTestTools as emt
 
-def RunObjectives(*args, **kwargs):
-    """RunObjectives
+from RunObjectives import DoTestScript
 
-    Runs trial (simulation, reconstruction,
-    and all analyses) for provided set of
-    updated parameters.
-
-    Args:
-      args:   any positional arguments
-      kwargs: any keyword arguments
-    Returns:
-      dictionary of objectives and their values
-    """
-
-
-    # create tag for trial
-    time = str(datetime.datetime.now())
-    time = re.sub(r'[.\-:\ ]', '', time)
-    tag = f"AxTrial{time}"
-
-    # extract path to script being run currently
-    main_path, main_file = emt.SplitPathAndFile(
-        os.path.realpath(__file__)
-    )
-
-    # determine paths to config files
-    #   -- FIXME this is brittle!
-    run_path  = main_path + "/configuration/run.config"
-    par_path  = main_path + "/configuration/parameters.config"
-    obj_path  = main_path + "/configuration/objectives.config"
-
-    # parse run config to extract path to eic-shell 
-    cfg_run   = emt.ReadJsonFile(run_path)
-    eic_shell = cfg_run["eic_shell"]
-
-    # create trial manager
-    trial = emt.TrialManager(run_path,
-                             par_path,
-                             obj_path)
-
-    # create and run script
-    script, ofiles = trial.MakeTrialScript(tag, kwargs)
-    subprocess.run([eic_shell, "--", script])
-
-    # extract electron resolution 
-    ofResEle = ofiles["ElectronEnergyResolution"].replace(".root", ".txt")
-    eResEle  = None
-    with open(ofResEle) as out:
-        eResEle = float(out.read().splitlines()[0])
-
-    # return dictionary of objectives
-    return {
-        "ElectronEnergyResolution" : eResEle
-    }
-
-def main():
+def main(*args, **kwargs):
     """main
 
     Wrapper to run BIC-MOBO. The model
@@ -89,22 +34,32 @@ def main():
     the -r option:
 
       joblib -- use joblib runner (default)
-      slurm  -- use slurm runner (TODO)
+      slurm  -- use slurm runner (in progress)
       panda  -- use panda runner (TODO)
 
     Args:
       -r: specify runner (optional)
     """
+
+    # set up arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--runner", help = "Runner type", nargs = '?', const = 1, type = str, default = "joblib")
+
+    # grab arguments
+    args = parser.parse_args()    
+
     # extract path to script being run currently
     #   - FIXME this should get automated!
     main_path, main_file = emt.SplitPathAndFile(
         os.path.realpath(__file__)
     )
-    exp_path  = main_path + "/configuration/problem.config"
-    par_path  = main_path + "/configuration/parameters.config"
-    obj_path  = main_path + "/configuration/objectives.config"
+    run_path = main_path + "/configuration/run.config"
+    exp_path = main_path + "/configuration/problem.config"
+    par_path = main_path + "/configuration/parameters.config"
+    obj_path = main_path + "/configuration/objectives.config"
 
     # load relevant config files
+    cfg_run = emt.ReadJsonFile(run_path)
     cfg_exp = emt.ReadJsonFile(exp_path)
     cfg_par = emt.ReadJsonFile(par_path)
     cfg_obj = emt.ReadJsonFile(obj_path)
@@ -122,14 +77,45 @@ def main():
         objectives = ax_objs
     )
 
+    # set up runners
+    runner = None
+    match args.runner:
+        case "joblib":
+            runner = JobLibRunner(
+                n_jobs = -1,
+                config = {
+                    'tmp_dir' : cfg_run["run_path"]
+                }
+            )
+        case "slurm":
+            runner = SlurmRunner(
+                partition     = "ifarm",
+                time_limit    = "00:30:00",  # FIXME bump up when ready!
+                memory        = "8G",
+                cpus_per_task = 4,
+                config        = {
+                    'modules' : ['python', 'singularity'],
+                    'sbatch_options' : {
+                        'mail-user' : 'dereka@jlab.org',
+                        'mail-type' : 'END,FAIL'
+                    }
+                }
+            )
+        case _:
+            raise ValueError("Unknown runner specified!")
+
     # set up scheduler
-    #   - TODO add switch to toggle slurm running
-    runner    = JobLibRunner(n_jobs = -1)
-    scheduler = AxScheduler(ax_client, runner)
-    scheduler.set_objective_function(RunObjectives)
+    scheduler = AxScheduler(
+        ax_client,
+        runner,
+        config = {
+            'job_output_dir' : cfg_exp["OUTPUT_DIR"],
+        }
+    )
+    scheduler.set_objective_function(DoTestScript)
 
     # run and report best parameters
-    best = scheduler.run_optimization(max_trials = 720)
+    best = scheduler.run_optimization(max_trials = 10)
     print("Optimization complete! Best parameters:\n", best)
 
     # create paths to output files
